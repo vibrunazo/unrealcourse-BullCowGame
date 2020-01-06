93461d63ae8f4ed221c556c9111f2818fd14f62c
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-01-06 11:35:52 -0300

    l47

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Hi There for instance!");
-    PrintLine("Input something.");
+    PrintLine(TEXT("Hi There for instance!"));
+    PrintLine(TEXT("Input something."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    PrintLine("Cleared.");
+    PrintLine(TEXT("Cleared."));
+    FString HiddenWord = TEXT("bunda");
 }
