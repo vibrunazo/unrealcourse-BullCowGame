aebf7c5afddc472c2426fee30d52908dcd9fc6ae
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-01-10 12:10:31 -0300

    l58

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,40 +5,46 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Hi There for instance!"));
-    PrintLine(TEXT("Input something."));
 
-    InitGame();
+    SetupGame();
 
+    // PrintLine(TEXT("The HiddenWord is: %s.\nIt is %i characters long"), *HiddenWord, HiddenWord.Len());
+    // PrintLine(FString::Printf(TEXT("The HiddenWord is: %s"), *HiddenWord));
     // prompts for a guess
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    // if life == 0, game over
-    if (Lives == 0) 
+
+    // if life == 0,  game over
+    if (Lives == 0)
     {
         PrintLine(TEXT("No lives left."));
         return;
     }
-    FString GuessWord = Input;
+
     if (HiddenWord == Input)
-    { 
+    {
         PrintLine(TEXT("YES"));
     }
     else
     {
         PrintLine(TEXT("NO"));
         Lives--;
+        if (HiddenWord.Len() != Input.Len())
+        {
+            PrintLine(TEXT("Hidden word is %i chars long."), HiddenWord.Len());
+        }
     }
     FString LivesText = TEXT("Lives left: ");
     LivesText.AppendInt(Lives);
     PrintLine(LivesText);
-    PrintLine(TEXT("Hidden word was:"));
-    PrintLine(HiddenWord);
+    PrintLine(TEXT("Hidden word was: %s"), *HiddenWord);
 
     // if life == 0, game over
-    if (Lives == 0) 
+    if (Lives == 0)
     {
         PrintLine(TEXT("GG Motherfucker."));
     }
@@ -47,8 +53,8 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     // else, quit terminal
 }
 
-void UBullCowCartridge::InitGame()
+void UBullCowCartridge::SetupGame()
 {
-    HiddenWord = TEXT("bunda");
+    HiddenWord = TEXT("macaco");
     Lives = 3;
 }
