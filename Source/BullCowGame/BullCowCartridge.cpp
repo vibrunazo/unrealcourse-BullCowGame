5b634d8fcd893f2541582b3220e59159d8235eae
Vandré Israel Brunazo <vibrunazo@gmail.com>
2020-01-10 10:58:42 -0300

    l55

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,23 +6,49 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
     PrintLine(TEXT("Hi There for instance!"));
     PrintLine(TEXT("Input something."));
-    HiddenWord = TEXT("bunda");
+
+    InitGame();
+
+    // prompts for a guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    PrintLine(TEXT("Cleared."));
+    // if life == 0, game over
+    if (Lives == 0) 
+    {
+        PrintLine(TEXT("No lives left."));
+        return;
+    }
     FString GuessWord = Input;
     if (HiddenWord == Input)
-    {
+    { 
         PrintLine(TEXT("YES"));
     }
     else
     {
         PrintLine(TEXT("NO"));
+        Lives--;
     }
-    
+    FString LivesText = TEXT("Lives left: ");
+    LivesText.AppendInt(Lives);
+    PrintLine(LivesText);
     PrintLine(TEXT("Hidden word was:"));
     PrintLine(HiddenWord);
+
+    // if life == 0, game over
+    if (Lives == 0) 
+    {
+        PrintLine(TEXT("GG Motherfucker."));
+    }
+    // prompt to play again
+    // if yes, call begin play
+    // else, quit terminal
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("bunda");
+    Lives = 3;
 }
